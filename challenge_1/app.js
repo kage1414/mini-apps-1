580874a6bad40b1f93be3d5905cd171f7553840f
kage1414 <kylejohnson92294@gmail.com>
2021-03-22 11:49:28 -0400

    Add click listener to all cells

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,7 +1,5 @@
-
 let table = document.createElement('table');
 document.body.appendChild(table);
-
 
 let i = 0;
 while (i < 3) {
@@ -11,14 +9,22 @@ while (i < 3) {
   while (j < 3) {
     let cell = document.createElement('td');
     cell.innerHTML = ' ';
-    cell.setAttribute('class', j)
-    cell.setAttribute('style', 'height: 200px; width: 200px; text-align: center; vertical-align: sub; font-size: 100px; background-color: #d9d9d9;')
+    cell.setAttribute('class', j);
+    cell.setAttribute('class', 'cell');
+    cell.setAttribute('id', i.toString() + j.toString());
+    cell.setAttribute('style', 'height: 200px; width: 200px; text-align: center; vertical-align: sub; font-size: 100px; background-color: #d9d9d9;');
     row.appendChild(cell);
     j++
   }
-
   row.setAttribute('class', i);
   table.appendChild(row);
-  console.log(i)
   i++;
+}
+
+var cells = document.getElementsByClassName('cell')
+
+for (let i = 0; i < cells.length; i++) {
+  cells[i].addEventListener('click', () => {
+    console.log('click');
+  })
 }
