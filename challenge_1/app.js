f1f642d883330cefdf0d5c03aadbeb81a1590fcb
kage1414 <kylejohnson92294@gmail.com>
2021-03-22 11:27:13 -0400

    Create board

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,6 +1,24 @@
 
-var table = document.createElement('table');
+let table = document.createElement('table');
 document.body.appendChild(table);
 
-// var row = document.createElement('tr');
-// table.appendChild(row);
+
+let i = 0;
+while (i < 3) {
+  let row = document.createElement('tr');
+  // row.innerHTML = ' ';
+  let j = 0;
+  while (j < 3) {
+    let cell = document.createElement('td');
+    cell.innerHTML = ' ';
+    cell.setAttribute('class', j)
+    cell.setAttribute('style', 'height: 200px; width: 200px; text-align: center; vertical-align: sub; font-size: 100px; background-color: #d9d9d9;')
+    row.appendChild(cell);
+    j++
+  }
+
+  row.setAttribute('class', i);
+  table.appendChild(row);
+  console.log(i)
+  i++;
+}
