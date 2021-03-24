5c281fc43055f99d467a5f32d23ca199e692ba46
kage1414 <kylejohnson92294@gmail.com>
2021-03-23 20:53:58 -0400

    Implementing API Request function

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,8 @@
+const url = '127.0.0.1/json';
 
+var xhr = new XMLHttpRequest();
 
+var sendRequest = () => {
+  xhr.open('GET', url);
+  xhr.send();
+};
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,23 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const app = express();
+const PORT = 3000;
 
+app.use('/index', express.static(path.join(__dirname, 'client', 'index.html')));
 
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log('Server listening on port', PORT);
+});
+
+app.get('/', (req, res, next) => {
+  res.redirect('/index');
+  next();
+});
+
+app.get('/json', (req, res, next) => {
+  res.send('hello');
+});
