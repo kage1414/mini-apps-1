27e86cfe34b1aa4e5cf2ba245986c307225a6d30
kage1414 <kylejohnson92294@gmail.com>
2021-03-25 10:18:23 -0400

    Move csv functions to csv.js

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const bodyParser = require('body-parser');
 const fileupload = require('express-fileupload');
+const csv = require('./csv.js');
 
 const app = express();
 const PORT = 3000;
@@ -19,68 +20,10 @@ app.get('/', (req, res, next) => {
   next();
 });
 
-let jsonToCsv = (json) => {
-  let parsed = JSON.parse(json);
-  let idxReference = [];
-  let keys = Object.keys(parsed);
-
-  _.map(keys, (key) => {
-    if (key !== 'children') {
-      idxReference.push(key);
-    }
-  });
-
-  // Render first line of csv
-  let lines = [];
-  lines.push(idxReference.join(','));
-
-  // Render subsequent lines of children
-
-  let writeValues = (json) => {
-    var valArray = [];
-
-    for (var key in json) {
-      if (key !== 'children') {
-        var idx = idxReference.indexOf(key);
-        if (idx === -1) {
-          idxReference.push(key);
-          idx = idxReference.indexOf(key);
-        }
-        valArray[idx] = json[key];
-      }
-    }
-
-    for (let i = 0; i < idxReference.length; i++) {
-      if (!valArray[i]) {
-        valArray[i] = '';
-      }
-    }
-
-    lines.push(valArray.join(','));
-
-    if (json.children.length > 0) {
-      _.each(json.children, (child) => {
-        writeValues(child);
-      });
-    }
-  };
-
-  writeValues(parsed);
-
-  return lines.join('\n');
-};
-
-let jsonFileToCsv = (json) => {
-  let string = json.data.toString();
-
-  return jsonToCsv(string);
-};
-
 app.post('/json', (req, res, next) => {
-  let csv = jsonFileToCsv(req.files.json);
+  let csvString = csv.jsonFileToCsv(req.files.json);
   res.type('text/csv');
-  console.log(csv)
-  res.send(csv);
+  res.send(csvString);
   next();
 });
 
