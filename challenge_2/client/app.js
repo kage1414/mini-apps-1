280a17b11fab1bcb2b527874b216f0ddba9bb5f7
kage1414 <kylejohnson92294@gmail.com>
2021-03-25 13:19:47 -0400

    Working on refactor saving most recent file

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,7 +1,3 @@
-let model = {
-  JSONurl: '/json'
-};
-
 let view = {
   appendFormToBody: () => {
     document.body.innerHTML =
@@ -9,20 +5,29 @@ let view = {
     <form id="submitJSON" enctype="multipart/form-data">
       <textarea id="json"></textarea>
       <input id="submit" type="submit" value="Submit">
-    </form>`;
+    </form>
+    <button id="latest">Get Latest File</button>`;
   }
 };
 
 let controller = {
   initialize: () => {
     view.appendFormToBody();
+
     $('#submitJSON').on('submit', (e) => {
       e.preventDefault();
       var jsonData = $('#json').val();
       $.ajax({
         method: 'POST',
-        url: model.JSONurl,
+        url: '/json',
         data: {json: jsonData}
+      });
+    });
+
+    $('#latest').on('click', (r) => {
+      $.ajax({
+        method: 'GET',
+        url: '/latest'
       });
     });
   }
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const _ = require('lodash');
 const path = require('path');
 const bodyParser = require('body-parser');
 const fileupload = require('express-fileupload');
+const controller = ('./controller.js');
 const csv = require('./csv.js');
+const _ = require('lodash');
+const fs = require('fs');
 
 const app = express();
 const PORT = 3000;
@@ -22,10 +24,27 @@ app.get('/', (req, res, next) => {
 app.post('/json', (req, res, next) => {
   // console.log(req.body);
   var csvData = csv.jsonToCsv(req.body.json);
-  console.log(csvData);
+  fs.writeFile('./latest.csv', csvData, (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+  // console.log(csvData);
   res.type('text/csv');
   res.send(csvData);
   next();
+});
+
+app.get('/latest', (req, res, next) => {
+  fs.readFile('./latest.csv', (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    res.type('text/csv');
+    res.attachment('latest.csv');
+    res.send(data);
+    next();
+  });
 });
 
 app.listen(PORT, (err) => {
