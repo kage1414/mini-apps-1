d5c9b2fa707dbb59010ab3abe3fa44de5e60e0e3
kage1414 <kylejohnson92294@gmail.com>
2021-04-12 15:27:15 -0400

    Handle input for first page

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -10,19 +10,56 @@ class App extends React.Component {
       submitted: false,
       page: 0,
     };
+    this.pages = {
+      page1: ['name', 'email', 'password'],
+      page2: ['line1', 'line2', 'city', 'state', 'shippingZip', 'phone'],
+      page3: ['cardNumber', 'expiry', 'cvv', 'billingZip']
+    };
   }
 
-  increment() {
+  nextPage() {
     this.setState({
       page: this.state.page + 1
     });
   }
 
+  handleInputChange(event) {
+    event.preventDefault();
+    let obj = {};
+    let key = event.target.id;
+    let value = event.target.value;
+    obj[key] = value;
+    this.setState(obj);
+  }
+
+  handleSubmit(event) {
+    this.setState({
+      error: false
+    });
+    event.preventDefault();
+
+    let pageNumber = event.target.id;
+    let inputs = this.pages[pageNumber];
+
+    for (let i = 0; i < inputs.length; i++) {
+      let input = inputs[i];
+      console.log(this.state)
+      if (!this.state[input]) {
+        this.setState({
+          error: true
+        });
+        console.log(inputs[i]);
+        return;
+      }
+    }
+
+    this.nextPage();
+  }
+
   render() {
     return (
       <div>
-        <button onClick={this.increment.bind(this)}/>
-        <Checkout page={this.state.page}/>
+        <Checkout handleSubmit={this.handleSubmit.bind(this)} page={this.state.page} handleInputChange={this.handleInputChange.bind(this)} />
       </div>
     );
   }
@@ -32,13 +69,22 @@ class Checkout extends React.Component {
 
   constructor(props) {
     super(props);
+    // this.handleInputChange = this.handleInputChange.bind(this);
   }
 
+  handleInputChange(event) {
+    event.preventDefault();
+    let obj = {};
+    let key = event.target.id;
+    let value = event.target.value;
+    obj[key] = value;
+    this.setState(obj);
+  }
 
   render() {
 
     const steps = [
-      <F1 />,
+      <F1 handleSubmit={this.props.handleSubmit} handleInputChange={this.props.handleInputChange} />,
       <F2 />,
       <F3 />
     ];
@@ -56,16 +102,18 @@ const F1 = (props) => {
 
   return (
     <div>
-      <form>
-        <span>Name: </span>
-        <input type="text" id="name"/>
+      <form onSubmit={props.handleSubmit} id="page1">
+        <label for="name">Name</label>
+        <input type="text" id="name" onChange={props.handleInputChange} />
         <br/>
-        <span>Email: </span>
-        <input type="text" id="email" />
-        <br/>
-        <span>Password: </span>
-        <input type="password" id="password" />
-      </form>
+        <label for="email">Email</label>
+        <input type="text" id="email" onChange={props.handleInputChange} />
+        <br />
+        <label for="name">Password</label>
+        <input type="password" id="password" onChange={props.handleInputChange} />
+        <br />
+        <input type="submit" ></input>
+      </form >
     </div>
   );
 };
--- challenge_3/client/script.js
+++ challenge_3/client/script.js
@@ -8,19 +8,57 @@ class App extends React.Component {
       submitted: false,
       page: 0
     };
+    this.pages = {
+      page1: ['name', 'email', 'password'],
+      page2: ['line1', 'line2', 'city', 'state', 'shippingZip', 'phone'],
+      page3: ['cardNumber', 'expiry', 'cvv', 'billingZip']
+    };
   }
 
-  increment() {
+  nextPage() {
     this.setState({
       page: this.state.page + 1
     });
   }
 
+  handleInputChange(event) {
+    event.preventDefault();
+    let obj = {};
+    let key = event.target.id;
+    let value = event.target.value;
+    obj[key] = value;
+    this.setState(obj);
+  }
+
+  handleSubmit(event) {
+    this.setState({
+      error: false
+    });
+    event.preventDefault();
+    let pageNumber = event.target.id;
+    let inputs = this.pages[pageNumber];
+
+    for (let i = 0; i < inputs.length; i++) {
+      let input = inputs[i];
+      console.log(this.state);
+
+      if (!this.state[input]) {
+        this.setState({
+          error: true
+        });
+        console.log(inputs[i]);
+        return;
+      }
+    }
+
+    this.nextPage();
+  }
+
   render() {
-    return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("button", {
-      onClick: this.increment.bind(this)
-    }), /*#__PURE__*/React.createElement(Checkout, {
-      page: this.state.page
+    return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement(Checkout, {
+      handleSubmit: this.handleSubmit.bind(this),
+      page: this.state.page,
+      handleInputChange: this.handleInputChange.bind(this)
     }));
   }
 
@@ -28,27 +66,53 @@ class App extends React.Component {
 
 class Checkout extends React.Component {
   constructor(props) {
-    super(props);
+    super(props); // this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    event.preventDefault();
+    let obj = {};
+    let key = event.target.id;
+    let value = event.target.value;
+    obj[key] = value;
+    this.setState(obj);
   }
 
   render() {
-    const steps = [/*#__PURE__*/React.createElement(F1, null), /*#__PURE__*/React.createElement(F2, null), /*#__PURE__*/React.createElement(F3, null)];
+    const steps = [/*#__PURE__*/React.createElement(F1, {
+      handleSubmit: this.props.handleSubmit,
+      handleInputChange: this.props.handleInputChange
+    }), /*#__PURE__*/React.createElement(F2, null), /*#__PURE__*/React.createElement(F3, null)];
     return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("span", null, "Checkout"), steps[this.props.page]);
   }
 
 }
 
 const F1 = props => {
-  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("span", null, "Name: "), /*#__PURE__*/React.createElement("input", {
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("form", {
+    onSubmit: props.handleSubmit,
+    id: "page1"
+  }, /*#__PURE__*/React.createElement("label", {
+    for: "name"
+  }, "Name"), /*#__PURE__*/React.createElement("input", {
     type: "text",
-    id: "name"
-  }), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("span", null, "Email: "), /*#__PURE__*/React.createElement("input", {
+    id: "name",
+    onChange: props.handleInputChange
+  }), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("label", {
+    for: "email"
+  }, "Email"), /*#__PURE__*/React.createElement("input", {
     type: "text",
-    id: "email"
-  }), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("span", null, "Password: "), /*#__PURE__*/React.createElement("input", {
+    id: "email",
+    onChange: props.handleInputChange
+  }), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("label", {
+    for: "name"
+  }, "Password"), /*#__PURE__*/React.createElement("input", {
     type: "password",
-    id: "password"
-  }));
+    id: "password",
+    onChange: props.handleInputChange
+  }), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("input", {
+    type: "submit"
+  })));
 };
 
 const F2 = props => {
