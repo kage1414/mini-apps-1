44864b053d2e632404128d983843f904f012ef42
kage1414 <kylejohnson92294@gmail.com>
2021-04-13 23:12:45 -0400

    Configure server

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,15 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+const PORT = 3000;
 
+const staticDir = path.join(__dirname, 'public');
 
+app.use(express.static(staticDir));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+app.listen(PORT, () => {
+  console.log('Listening on port:', PORT);
+});
