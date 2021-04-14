dd14b389cce5136c2500942f406d25862573fca1
kage1414 <kylejohnson92294@gmail.com>
2021-04-13 23:48:20 -0400

    Configure frontend

--- challenge_4/client/app.jsx
+++ challenge_4/client/app.jsx
@@ -1,2 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Test Test Test</h1>
+        <Table />
+      </div>
+    );
+  }
+}
+
+class Table extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h2>SubTest</h2>
+    );
+  }
+
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- challenge_4/webpack.config.js
+++ challenge_4/webpack.config.js
@@ -1,10 +1,24 @@
 const path = require('path');
 
 module.exports = {
-  entry: './client/app.jsx',
+  entry: `${path.join(__dirname, 'client')}/app.jsx`,
   output: {
     filename: 'app.js',
     path: path.resolve(__dirname, 'public')
   },
-  mode: 'production'
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: [/\.jsx$/],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
 };
